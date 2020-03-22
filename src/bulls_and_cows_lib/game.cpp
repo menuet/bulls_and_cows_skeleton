0d35c8ea717d368ac4f1d99638cd20954b9dccd6
Louis <louis.potterie@efrei.net>
2020-03-22 16:52:22 +0100

    Version alpha

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -5,12 +5,163 @@
 #include "game_solver.hpp"
 #include "input.hpp"
 #include "main_menu.hpp"
+#include "random.hpp"
+#include <vector> 
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <thread>
-
+using namespace std;
 namespace bulls_and_cows {
+
+	int distinct(char rand, std::vector <char> secretCode)
+	{
+		int vrai = 0;
+		for (int i = 0; i < secretCode.size(); i++)
+		{
+			if (rand == secretCode[i])
+			{
+				vrai = 1;
+			}
+		}
+	
+		return vrai;
+	}
+
+	
+	std::vector<char> secret_code_init(std::vector <char> secretCode)
+	{
+
+		GameOptions option;
+		char min = option.minimum_allowed_character;
+		char max = option.maximum_allowed_character;
+		char rand = generate_random_character(min, max);
+		int l = 0;
+		
+		while (l < secretCode.size())
+		{
+			
+			for (int i = 0; i < secretCode.size(); i++)
+			{
+				
+				while (distinct(rand, secretCode) == 1)
+				{
+					rand = generate_random_character(min, max);
+					distinct(rand, secretCode);
+					
+				}
+				
+				secretCode[i] = rand;
+			
+			}
+			
+			std::cout << secretCode[l];
+			l++;
+		}
+	
+		return secretCode;
+	}
+
+	vector<char> secret_code_init_V2(vector<char> secretCode)
+	{
+		
+		GameOptions option;
+		char min = option.minimum_allowed_character;
+		char max = option.maximum_allowed_character;
+		char rand;
+
+		for (int i = 0; i < secretCode.size(); i++)
+		{
+			rand = generate_random_character(min, max);
+			while (rand == secretCode[0] || rand == secretCode[1] || rand == secretCode[2] || rand == secretCode[3] || rand == secretCode[4])
+			{
+				rand = generate_random_character(min, max);
+			}
+			secretCode[i] = rand;
+		}
+
+		for (int z = 0; z<secretCode.size(); z++)
+		{
+			cout << secretCode[z];
+		}
+
+		return secretCode;
+
+	}
+
+	void display_board(std::vector <std::vector <std::string>> board)
+	{
+		for (int i = 0; i < board.size(); i++) {
+			std::cout << "\n";
+			for (int j = 0; j < board[i].size(); j++)
+				std::cout << board[i][j] << " ";
+
+		}
+	}
+
+	std::vector<char> secret_code_player(std::vector <char> playerCode)
+	{
+		std::istream& input_stream = std::cin;
+		std::cout << "\n Give a secret code character by character \n";
+
+		for (int i = 0; i < playerCode.size(); i++)
+		{
+			std::cout << "Letter  : ";
+			playerCode[i] = ask_char_or_default(input_stream, 'O');
+
+
+		}
+
+		for (int i = 0; i < playerCode.size(); i++)
+		{
+
+			std::cout << playerCode[i];
+
+		}
+
+		return playerCode;
+
+	}
+
+	std::vector<int> count_true_false(std::vector <char> secretCode, std::vector <char> playerCode, std::vector<int> counter_bulls_cows)
+	{
+		int bulls = 0;
+		int cows = 0; 
+
+		for (int i = 0; i<playerCode.size(); i++)
+		{
+			if (secretCode[i] == playerCode[i])
+			{
+				bulls = bulls + 1; 
+			}
+			
+			for (int j = 0; j < playerCode.size(); j++)
+			{
+				
+				if (secretCode[j] == playerCode[i])
+				{
+					cows = cows + 1; 
+				}
+			}
+			
+		}
+		
+		if (bulls == secretCode.size())
+		{
+			counter_bulls_cows[0] = 1;
+		}
+		else
+		{
+			counter_bulls_cows[0] = 0;
+		}
+
+		cows = cows - bulls;
+		counter_bulls_cows[1] = bulls;
+		counter_bulls_cows[2] = cows;
+
+		return counter_bulls_cows;
+
+	}
 
     void user_plays_against_computer(const GameOptions& game_options)
     {
@@ -24,6 +175,62 @@ namespace bulls_and_cows {
                      "    WHILE not end of game\n"
                      "    Display the board and the list of attempts so far\n"
                      "    Display a message telling if the user won or lost\n";
+
+
+		GameOptions option;
+		std::vector <char> secretCode(option.number_of_characters_per_code);
+		secretCode = secret_code_init_V2(secretCode);
+		std::vector <std::vector <std::string>> board{ {"--","-------", "---" ,"----", "---" ,"-----", "--", "--", ""}, {"|","Attempts", "||" ,"Code", "||" ,"Bulls", "||", "Cows", "|"}, {"|","  1    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  2    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  3    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  4    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  5    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  6    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  7    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  8    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  9    ", "||" ,"    ", "||" ,"     ","||", "    ","|"} , {"|","  10   ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  11   ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  12   ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {" --","-------", "---" ,"----", "---" ,"-----", "--", "--", "" } };
+		std::vector <char> playerCode(option.number_of_characters_per_code);
+		std::vector<int> counter_bowls_cows(3);
+		//std::vector <std::vector <std::string>> board{ {"--","-------", "---" ,"----", "---" ,"-----", "--", "--", ""}, {"|","Attempts", "||" ,"Code", "||" ,"Bulls", "||", "Cows", "|"}};
+		
+		unsigned int player_attempts = 0;
+		int line_table = 0;
+		int win = 0;
+		
+		
+		display_board(board);
+	
+		
+		while (player_attempts < option.max_number_of_attempts && win != 1)
+		{
+			string concat;
+			line_table = player_attempts+2;
+			playerCode = secret_code_player(playerCode);
+			counter_bowls_cows = count_true_false(secretCode, playerCode, counter_bowls_cows);
+			//display_board(board);
+			concat += playerCode[0];
+			concat += playerCode[1];
+			concat += playerCode[2];
+			concat += playerCode[3];
+			concat += playerCode[4];
+			//board.push_back({ concat,to_string(counter_bowls_cows[2]),to_string(counter_bowls_cows[3]) });
+			board[line_table][3] = concat;
+			board[line_table][5] = to_string(counter_bowls_cows[1]);
+			board[line_table][7] = to_string(counter_bowls_cows[2]);
+			display_board(board);
+
+			if (counter_bowls_cows[1] == 5)
+			{
+				win = 1;
+			}
+
+			player_attempts = player_attempts + 1;
+
+		}
+
+		if (win == 1)
+		{
+			cout<< "YOU WIN !";
+		}
+		
+		else
+		{
+			cout << "YOU LOOSE ! ";
+		}
+		
+
     }
 
     void computer_plays_against_computer(const GameOptions& game_options)
