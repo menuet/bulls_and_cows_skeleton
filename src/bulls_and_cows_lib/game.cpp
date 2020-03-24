b47e9119dedb45c8b857787ca3cbd1b25a895a7e
Kenza Aoun <kenzap16@gmail.com>
2020-03-24 08:34:49 +0100

    test main menu in game.cpp

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -5,6 +5,7 @@
 #include "game_solver.hpp"
 #include "input.hpp"
 #include "main_menu.hpp"
+#include "random.hpp"
 #include <chrono>
 #include <fstream>
 #include <iostream>
@@ -24,6 +25,270 @@ namespace bulls_and_cows {
                      "    WHILE not end of game\n"
                      "    Display the board and the list of attempts so far\n"
                      "    Display a message telling if the user won or lost\n";
+        char motsecret[5];
+        char motrecuperer[13][5];
+        int i,j;
+
+       
+        for (i = 0; i < 5; i++)
+        {
+            motsecret[i] = generate_random_character(65, 72 );
+            std::cout << motsecret[i];
+        }
+
+
+         std::cout << "\n\nLets Play!!\n\n"
+                     "-----------------------------------------\n"
+                     "| Secret       * * * * * |              |\n"
+                     "-----------------------------------------\n"
+                     "|ATTEMPTS                | BULLS | COWS |\n"
+                     "-----------------------------------------\n"
+                     "| #12          - - - - - |       |      |\n"
+                     "| #11          - - - - - |       |      |\n"
+                     "| #10          - - - - - |       |      |\n"
+                     "| #9           - - - - - |       |      |\n"
+                     "| #8           - - - - - |       |      |\n"
+                     "| #7           - - - - - |       |      |\n"
+                     "| #6           - - - - - |       |      |\n"
+                     "| #5           - - - - - |       |      |\n"
+                     "| #4           - - - - - |       |      |\n"
+                     "| #3           - - - - - |       |      |\n"
+                     "| #2           - - - - - |       |      |\n"
+                     "| #1           - - - - - |       |      |\n"
+                     "-----------------------------------------\n"
+                     "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+       
+        for (i = 0; i < 12; i++)
+         {
+            for (j = 0; j < 5;j++)
+            {
+                motrecuperer[i][j] = '- ';
+            }
+         }
+         
+         
+         for (i = 0; i < 12; i++)
+        {
+           std::cout << "\n\n"
+                        "-----------------------------------------\n"
+                        "| Secret       * * * * * |              |\n"
+                        "-----------------------------------------\n"
+                        "|ATTEMPTS                | BULLS | COWS |\n"
+                        "-----------------------------------------\n"
+                        "| #12          - - - - - |       |      |\n"
+                        "| #11          - - - - - |       |      |\n"
+                        "| #10          - - - - - |       |      |\n"
+                        "| #9           - - - - - |       |      |\n"
+                        "| #8           - - - - - |       |      |\n"
+                        "| #7           - - - - - |       |      |\n"
+                        "| #6           - - - - - |       |      |\n"
+                        "| #5           - - - - - |       |      |\n"
+                        "| #4           - - - - - |       |      |\n"
+                        "| #3           - - - - - |       |      |\n"
+                        "| #2           - - - - - |       |      |\n"
+                        "| #1           - - - - - |       |      |\n"
+                        "-----------------------------------------\n"
+                        "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+
+          
+           
+           
+           switch (i)
+           {
+           case 0:
+               std::cout << "\n\n"
+                            "-----------------------------------------\n"
+                            "| Secret       * * * * * |              |\n"
+                            "-----------------------------------------\n"
+                            "|ATTEMPTS                | BULLS | COWS |\n"
+                            "-----------------------------------------\n"
+                            "| #12          - - - - - |       |      |\n"
+                            "| #11          - - - - - |       |      |\n"
+                            "| #10          - - - - - |       |      |\n"
+                            "| #9           - - - - - |       |      |\n"
+                            "| #8           - - - - - |       |      |\n"
+                            "| #7           - - - - - |       |      |\n"
+                            "| #6           - - - - - |       |      |\n"
+                            "| #5           - - - - - |       |      |\n"
+                            "| #4           - - - - - |       |      |\n"
+                            "| #3           - - - - - |       |      |\n"
+                            "| #2           - - - - - |       |      |\n"
+                            "| #1          " +
+                                motrecuperer[i]
+                         << " |       |      |\n"
+                            "-----------------------------------------\n"
+                            "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+               break;
+           case 1:
+               for (j = 0; j < 5; j++)
+               {
+                  motrecuperer[i][j] = generate_random_character(65, 72);
+               }
+
+               std::cout << "\n\n"
+                            "-----------------------------------------\n"
+                            "| Secret       * * * * * |              |\n"
+                            "-----------------------------------------\n"
+                            "|ATTEMPTS                | BULLS | COWS |\n"
+                            "-----------------------------------------\n"
+                            "| #12          - - - - - |       |      |\n"
+                            "| #11          - - - - - |       |      |\n"
+                            "| #10          - - - - - |       |      |\n"
+                            "| #9           - - - - - |       |      |\n"
+                            "| #8           - - - - - |       |      |\n"
+                            "| #7           - - - - - |       |      |\n"
+                            "| #6           - - - - - |       |      |\n"
+                            "| #5           - - - - - |       |      |\n"
+                            "| #4           - - - - - |       |      |\n"
+                            "| #3           - - - - - |       |      |\n"
+                            "| #2          "+motrecuperer[i]<<" |       |      |\n"
+                            "| #1          "+motrecuperer[i-1]<<" |       |      |\n"
+                            "-----------------------------------------\n"
+                            "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+
+               break;
+           case 2:
+               std::cout << "\n\n"
+                            "-----------------------------------------\n"
+                            "| Secret       * * * * * |              |\n"
+                            "-----------------------------------------\n"
+                            "|ATTEMPTS                | BULLS | COWS |\n"
+                            "-----------------------------------------\n"
+                            "| #12          - - - - - |       |      |\n"
+                            "| #11          - - - - - |       |      |\n"
+                            "| #10          - - - - - |       |      |\n"
+                            "| #9           - - - - - |       |      |\n"
+                            "| #8           - - - - - |       |      |\n"
+                            "| #7           - - - - - |       |      |\n"
+                            "| #6           - - - - - |       |      |\n"
+                            "| #5           - - - - - |       |      |\n"
+                            "| #4           - - - - - |       |      |\n"
+                            "| #3          " +
+                                motrecuperer[i]
+                         << " |       |      |\n"
+                            "| #2          " +
+                                motrecuperer[i-1]
+                         << " |       |      |\n"
+                            "| #1          " +
+                                motrecuperer[i - 2]
+                         << " |       |      |\n"
+                            "-----------------------------------------\n"
+                            "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+               break;
+           case 3:
+               for (j = 0; j < 5; j++)
+               {
+                   motrecuperer[i][j] = generate_random_character(65, 72);
+               }
+               std::cout << "\n\n"
+                            "-----------------------------------------\n"
+                            "| Secret       * * * * * |              |\n"
+                            "-----------------------------------------\n"
+                            "|ATTEMPTS                | BULLS | COWS |\n"
+                            "-----------------------------------------\n"
+                            "| #12          - - - - - |       |      |\n"
+                            "| #11          - - - - - |       |      |\n"
+                            "| #10          - - - - - |       |      |\n"
+                            "| #9           - - - - - |       |      |\n"
+                            "| #8           - - - - - |       |      |\n"
+                            "| #7           - - - - - |       |      |\n"
+                            "| #6           - - - - - |       |      |\n"
+                            "| #5           - - - - - |       |      |\n"
+                            "| #4          " +
+                                motrecuperer[i]
+                         << "|       |      |\n"
+                            "| #3          " +
+                                motrecuperer[i-1]
+                         << " |       |      |\n"
+                            "| #2          " +
+                                motrecuperer[i - 2]
+                         << " |       |      |\n"
+                            "| #1          " +
+                                motrecuperer[i - 3]
+                         << " |       |      |\n"
+                            "-----------------------------------------\n"
+                            "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+               break;
+           case 4:
+
+
+               std::cout << "\n\n"
+                            "-----------------------------------------\n"
+                            "| Secret       * * * * * |              |\n"
+                            "-----------------------------------------\n"
+                            "|ATTEMPTS                | BULLS | COWS |\n"
+                            "-----------------------------------------\n"
+                            "| #12          - - - - - |       |      |\n"
+                            "| #11          - - - - - |       |      |\n"
+                            "| #10          - - - - - |       |      |\n"
+                            "| #9           - - - - - |       |      |\n"
+                            "| #8           - - - - - |       |      |\n"
+                            "| #7           - - - - - |       |      |\n"
+                            "| #6           - - - - - |       |      |\n"
+                            "| #5           - - - - - |       |      |\n"
+                            "| #4          " +
+                              motrecuperer[i-1]
+                         << "|       |      |\n"
+                            "| #3          " +
+                                motrecuperer[i - 2]
+                         << " |       |      |\n"
+                            "| #2          " +
+                                motrecuperer[i - 3]
+                         << " |       |      |\n"
+                            "| #1          " +
+                                motrecuperer[i - 4]
+                         << " |       |      |\n"
+                            "-----------------------------------------\n"
+                            "Now is your turn to guess #01(5 characters between 'A' and 'H')\n?";
+
+               break;
+           case 5:
+               for (j = 0; j < 5; j++)
+               {
+                   motrecuperer[i][j] = generate_random_character(65, 72);
+               }
+               break;
+           case 6:
+               break;
+           case 7:
+               for (j = 0; j < 5; j++)
+               {
+                   motrecuperer[i][j] = generate_random_character(65, 72);
+               }
+               break;
+           case 8:
+               break;
+           case 9:
+               for (j = 0; j < 5; j++)
+               {
+                   motrecuperer[i][j] = generate_random_character(65, 72);
+               }
+               break;
+           case 10:
+               break;
+           case 11:
+               for (j = 0; j < 5; j++)
+               {
+                   motrecuperer[i][j] = generate_random_character(65, 72);
+               }
+               break;
+           default:
+               break;
+           }
+           
+           
+           
+           
+        
+
+        }
+      
+       
+        
+
+
+       
+        
     }
 
     void computer_plays_against_computer(const GameOptions& game_options)
