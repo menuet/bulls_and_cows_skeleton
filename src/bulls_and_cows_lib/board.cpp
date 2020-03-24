4e1b9435637cb7b0932e420069c66aea44397b1a
Manal Jafjaf <manal.jafjaf@efrei.net>
2020-03-25 00:08:57 +0100

    fichier board debut

--- src/bulls_and_cows_lib/board.cpp
+++ src/bulls_and_cows_lib/board.cpp
@@ -1,8 +1,38 @@
 
 #include "board.hpp"
+#include "random.cpp"
 
 namespace bulls_and_cows {
 
     // TODO: define the body of the functions declared in board.cpp
+    Board create_board(const GameOptions& game_options)
+    {
+        Board board;
+        std::string code{};
+        for (unsigned i = 0; i < game_options.number_of_characters_per_code; i++)
+        {
+            code = code + generate_random_character(game_options.minimum_allowed_character,
+                                                    game_options.maximum_allowed_character);
+        }
+        board.secret_code.value = code;
+        return board;
+    }
 
+    bool validate_attempt(const GameOptions& game_options, const Code& attempt)
+    {
+        for (unsigned i = 0; i < game_options.number_of_characters_per_code; i++)
+        {
+            if (attempt.value[i] < game_options.minimum_allowed_character ||
+                attempt.value[i] > game_options.maximum_allowed_character)
+            {
+                return false;
+            }
+        }
+        if (attempt.value.size() != game_options.number_of_characters_per_code)
+        {
+            return false;
+        }
+        return true;
+    }
 } // namespace bulls_and_cows
+
