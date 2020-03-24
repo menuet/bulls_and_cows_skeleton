2862540ca2d5870becd86fb0d12239991077f8b7
Bilel <b.rimani@epmistes.net>
2020-03-24 12:16:36 +0100

    test

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -24,6 +24,81 @@ namespace bulls_and_cows {
                      "    WHILE not end of game\n"
                      "    Display the board and the list of attempts so far\n"
                      "    Display a message telling if the user won or lost\n";
+        
+
+        // créer une chaine de caractère de 10 lettres aux hasards
+
+        using namespace std;
+        char tt[27]; // créer un tableau de chaque lettre
+        tt[0] = 'A';
+        tt[1] = 'B';
+        tt[2] = 'C';
+        tt[3] = 'D';
+        tt[4] = 'E';
+        tt[5] = 'F';
+        tt[6] = 'G';
+        tt[7] = 'H';
+        tt[8] = 'I';
+        tt[9] = 'J';
+        tt[10] = 'K';
+        tt[11] = 'L';
+        tt[12] = 'M';
+        tt[13] = 'N';
+        tt[14] = 'O';
+        tt[15] = 'P';
+        tt[16] = 'Q';
+        tt[17] = 'R';
+        tt[18] = 'S';
+        tt[19] = 'T';
+        tt[20] = 'U';
+
+        tt[21] = 'V';
+        tt[22] = 'W';
+        tt[23] = 'X';
+        tt[24] = 'Y';
+        tt[25] = 'Z';
+
+        char tableauCacher[10];
+
+        for (int i = 0; i < 10; i++) 
+        {     
+    
+            int z = 0;        // initialise un nombre z
+            z = (rand()%25); // prendre un nombre z aléatoirement compris entre 1-26
+            // cout << tt[z]; // pour chaque chiffre que prendra z on aura une lettre du tableau cout << tt[z]
+            tableauCacher[i] = tt[z];
+            //cout << tableauCacher[i];       
+        }
+        // fonction permettant de compter le nombre de pion bien et mal placé (compteur1 et 2 pacé par reference)
+
+        // si une case de ces deux tableaux passe à vrai, alors elle permettent de ne plus revenir dessus (utilisé pour
+        // les éléments deja compté)
+        bool tabCacherValide[10]; 
+        bool tabChoixValide[10];
+        int tabChoix[10];
+        int compteur1 = 0; // compteur de pions bien placer
+
+        // compte ne nombre d'éléments bien placé
+        for (int i = 0; i < 10; i++)
+        {
+            if (tableauCacher[i] == tabChoix[i])
+            {
+                tabChoixValide[i] = true;
+                tabCacherValide[i] = true;
+                compteur1++;
+            }
+            else
+            {
+                tabChoixValide[i] = false;
+                tabCacherValide[i] = false;
+            }
+        }
+
+
+       // printf("entrer vos valeurs");
+        //scanf();
+        //char TableauTemp[10];
+        //for ( )
     }
 
     void computer_plays_against_computer(const GameOptions& game_options)
