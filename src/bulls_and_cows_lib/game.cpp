0d156d1c6fe6262211324def7bdc1033e3be853e
Louis <louis.potterie@efrei.net>
2020-03-24 22:57:52 +0100

    version beta

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -14,27 +14,151 @@
 using namespace std;
 namespace bulls_and_cows {
 
-	int distinct(char rand, std::vector <char> secretCode)
+	/*Validation ask_SecretCodeChoice(std::istream& input_stream, Validation valid)
 	{
-		int vrai = 0;
-		for (int i = 0; i < secretCode.size(); i++)
+		const char user_choice = ask_char_or_default(input_stream, -1);
+		switch (user_choice)
 		{
-			if (rand == secretCode[i])
+		case 'A':
+			valid.carac = 'A';
+			valid.SecretCodechoic;
+			return valid;
+		case 'B':
+			return SecretCodeChoice::Valid;
+		case 'C':
+			return SecretCodeChoice::Valid;
+		case 'D':
+			return SecretCodeChoice::Valid;
+		case 'E':
+			return SecretCodeChoice::Valid;
+		case 'F':
+			return SecretCodeChoice::Valid;
+		case 'G':
+			return SecretCodeChoice::Valid;
+		case 'H':
+			return SecretCodeChoice::Valid;
+			/*case 'I':
+				return SecretCodeChoice::Valid;
+			case 'J':
+				return SecretCodeChoice::Valid;
+			case 'K':
+				return SecretCodeChoice::Valid;
+			case 'L':
+				return SecretCodeChoice::Valid;
+			case 'M':
+				return SecretCodeChoice::Valid;
+			case 'N':
+				return SecretCodeChoice::Valid;
+			case 'O':
+				return SecretCodeChoice::Valid;
+			case 'P':
+				return SecretCodeChoice::Valid;
+			case 'Q':
+				return SecretCodeChoice::Valid;
+			case 'R':
+				return SecretCodeChoice::Valid;
+			case 'S':
+				return SecretCodeChoice::Valid;
+			case 'T':
+				return SecretCodeChoice::Valid;
+			case 'U':
+				return SecretCodeChoice::Valid;
+			case 'V':
+				return SecretCodeChoice::Valid;
+			case 'W':
+				return SecretCodeChoice::Valid;
+			case 'X':
+				return SecretCodeChoice::Valid;
+			case 'Y':
+				return SecretCodeChoice::Valid;
+			case 'Z':
+				return SecretCodeChoice::Valid;
+			}
+			return SecretCodeChoice::Error;
+		}
+	}*/
+
+	bool isContainedIn(char rand, const std::vector <char> secretCode)
+	{
+		bool is_found = false;
+		for (const char c : secretCode)
+		{
+			if (rand == c)
 			{
-				vrai = 1;
+				is_found = true;
 			}
 		}
 	
-		return vrai;
+		return is_found;
 	}
 
-	
-	std::vector<char> secret_code_init(std::vector <char> secretCode)
+
+	OptionMenuChoice ask_option_menu_choice(std::istream& input_stream)
+	{
+		const int user_choice = ask_int_or_default(input_stream, -1);
+		switch (user_choice)
+		{
+		case 0:
+			return OptionMenuChoice::Quit;
+		case 1:
+			return OptionMenuChoice::NumberOfCharacters;
+		case 2:
+			return OptionMenuChoice::CharMin;
+		case 3:
+			return OptionMenuChoice::CharMax;
+		}
+		return OptionMenuChoice::Error;
+	}
+
+	// L'utilisation de Template est surement une notion utile pour eviter de repeter le meme code 
+	ConfigureOptions ask_configuration_menu_choice(std::istream& input_stream)
+	{
+		const int user_choice = ask_int_or_default(input_stream, -1);
+		switch (user_choice)
+		{
+		case 0:
+			return ConfigureOptions::Quit;
+		case 1:
+			return ConfigureOptions::CurrentGameOptions;
+		case 2:
+			return ConfigureOptions::GameOptionMenu;
+		
+		}
+		return ConfigureOptions::Error;
+	}
+
+
+
+
+	bool checkError(char carac, const GameOptions& game_options)
+	{
+		bool check = false;
+		if (carac > game_options.maximum_allowed_character || carac < game_options.minimum_allowed_character)
+		{
+			std::cout << "Reselect a letter in the range \n";
+			check = true;
+		}
+
+		return check;
+	}
+
+
+
+	// Algorithme standard à tester
+	bool isContainedIn_V2(char characterToSearchFor, const std::vector<char> charactersToSearchIn)
+	{
+		const auto iter = std::find(charactersToSearchIn.begin(), charactersToSearchIn.end(), characterToSearchFor);
+		return iter != charactersToSearchIn.end();
+	}
+
+
+	//Function to initialize secre code 
+	std::vector<char> secret_code_init(std::vector <char> secretCode, const GameOptions& option)
 	{
 
-		GameOptions option;
-		char min = option.minimum_allowed_character;
-		char max = option.maximum_allowed_character;
+		
+		const char min = option.minimum_allowed_character;
+		const char max = option.maximum_allowed_character;
 		char rand = generate_random_character(min, max);
 		int l = 0;
 		
@@ -44,28 +168,24 @@ namespace bulls_and_cows {
 			for (int i = 0; i < secretCode.size(); i++)
 			{
 				
-				while (distinct(rand, secretCode) == 1)
+				while (isContainedIn(rand, secretCode))
 				{
 					rand = generate_random_character(min, max);
-					distinct(rand, secretCode);
 					
 				}
 				
 				secretCode[i] = rand;
 			
 			}
-			
-			std::cout << secretCode[l];
 			l++;
 		}
 	
 		return secretCode;
 	}
 
-	vector<char> secret_code_init_V2(vector<char> secretCode)
+	vector<char> secret_code_init_V2(vector<char> secretCode, const GameOptions& option)
 	{
 		
-		GameOptions option;
 		char min = option.minimum_allowed_character;
 		char max = option.maximum_allowed_character;
 		char rand;
@@ -89,25 +209,43 @@ namespace bulls_and_cows {
 
 	}
 
-	void display_board(std::vector <std::vector <std::string>> board)
+	void display_board(vector <FinalBoard>  board)
 	{
+		
+		
+		std::cout << "|";
+		std::cout << "Attempts" <<"||";
+		std::cout << "Code" <<" " << "||";
+		std::cout << "Bulls" << "||";
+		std::cout << "Cows" << "|";
+		
 		for (int i = 0; i < board.size(); i++) {
 			std::cout << "\n";
-			for (int j = 0; j < board[i].size(); j++)
-				std::cout << board[i][j] << " ";
-
+			std::cout << "|";
+			std::cout << "   " << i << "    " << "||";
+			std::cout << " " << board[i].secretCode << "||";
+			std::cout << "   " << board[i].bulls << "||";
+			std::cout << "   " << board[i].cows << "|";
 		}
 	}
 
-	std::vector<char> secret_code_player(std::vector <char> playerCode)
+	std::vector<char> secret_code_player(std::vector <char> playerCode, const GameOptions& game_options)
 	{
 		std::istream& input_stream = std::cin;
 		std::cout << "\n Give a secret code character by character \n";
 
+
+		
 		for (int i = 0; i < playerCode.size(); i++)
 		{
-			std::cout << "Letter  : ";
-			playerCode[i] = ask_char_or_default(input_stream, 'O');
+			bool check = false;
+			do {
+
+				std::cout << "Letter  : ";
+				playerCode[i] = ask_char_or_default(input_stream, 'O');
+				check = checkError(playerCode[i], game_options);
+
+			} while (check);
 
 
 		}
@@ -163,6 +301,18 @@ namespace bulls_and_cows {
 
 	}
 
+	string conv_vector_to_string(vector <char> vector_to_convert)
+	{
+		string chaine; 
+
+		for (const char c : vector_to_convert)
+		{
+			chaine += c;
+		}
+
+		return chaine;
+	}
+
     void user_plays_against_computer(const GameOptions& game_options)
     {
         std::cout << "TODO:\n"
@@ -179,57 +329,51 @@ namespace bulls_and_cows {
 
 		GameOptions option;
 		std::vector <char> secretCode(option.number_of_characters_per_code);
-		secretCode = secret_code_init_V2(secretCode);
-		std::vector <std::vector <std::string>> board{ {"--","-------", "---" ,"----", "---" ,"-----", "--", "--", ""}, {"|","Attempts", "||" ,"Code", "||" ,"Bulls", "||", "Cows", "|"}, {"|","  1    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  2    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  3    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  4    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  5    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  6    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  7    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  8    ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  9    ", "||" ,"    ", "||" ,"     ","||", "    ","|"} , {"|","  10   ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  11   ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {"|","  12   ", "||" ,"    ", "||" ,"     ","||", "    ","|"}, {" --","-------", "---" ,"----", "---" ,"-----", "--", "--", "" } };
+		secretCode = secret_code_init(secretCode, option);
 		std::vector <char> playerCode(option.number_of_characters_per_code);
 		std::vector<int> counter_bowls_cows(3);
-		//std::vector <std::vector <std::string>> board{ {"--","-------", "---" ,"----", "---" ,"-----", "--", "--", ""}, {"|","Attempts", "||" ,"Code", "||" ,"Bulls", "||", "Cows", "|"}};
-		
 		unsigned int player_attempts = 0;
-		int line_table = 0;
-		int win = 0;
+		bool not_win = true;
+		
+		string secret_code_to_find = conv_vector_to_string(secretCode);
+
+		cout << secret_code_to_find;
 		
 		
-		display_board(board);
-	
-		
-		while (player_attempts < option.max_number_of_attempts && win != 1)
+		vector <FinalBoard> board_final;
+		board_final.push_back(FinalBoard(secret_code_to_find, 0, 0));
+
+
+		do
 		{
-			string concat;
-			line_table = player_attempts+2;
-			playerCode = secret_code_player(playerCode);
+
+			playerCode = secret_code_player(playerCode, game_options);
+			string secret_code_player = conv_vector_to_string(playerCode);
 			counter_bowls_cows = count_true_false(secretCode, playerCode, counter_bowls_cows);
-			//display_board(board);
-			concat += playerCode[0];
-			concat += playerCode[1];
-			concat += playerCode[2];
-			concat += playerCode[3];
-			concat += playerCode[4];
-			//board.push_back({ concat,to_string(counter_bowls_cows[2]),to_string(counter_bowls_cows[3]) });
-			board[line_table][3] = concat;
-			board[line_table][5] = to_string(counter_bowls_cows[1]);
-			board[line_table][7] = to_string(counter_bowls_cows[2]);
-			display_board(board);
+			board_final.emplace_back(secret_code_player, counter_bowls_cows[1], counter_bowls_cows[2]);
+			player_attempts++;
+			cout << "\n";
+			display_board(board_final);
 
 			if (counter_bowls_cows[1] == 5)
 			{
-				win = 1;
+				not_win = false;
 			}
 
-			player_attempts = player_attempts + 1;
 
-		}
+		} while (not_win && player_attempts < option.max_number_of_attempts);
 
-		if (win == 1)
+
+		if (not_win == false)
 		{
-			cout<< "YOU WIN !";
+			cout << "\n" << "\n" << "YOU WIN !";
 		}
-		
+
 		else
 		{
 			cout << "YOU LOOSE ! ";
 		}
-		
+
 
     }
 
@@ -253,15 +397,129 @@ namespace bulls_and_cows {
                "    Display a message telling if the computer won or lost\n";
     }
 
+
+	void display_current_game_options(const GameOptions& game_options)
+	{
+		cout << "Game options : \n ";
+		cout << " Number of characters per code : " << game_options.number_of_characters_per_code;
+		cout << " \n Character min " << game_options.minimum_allowed_character;
+		cout << "\n Character max " << game_options.maximum_allowed_character;
+
+	}
+
+
+	GameOptions change_Number_Characters(GameOptions& game_options)
+	{
+		
+		cout << "How many characters do you want ? ";
+		const unsigned int numberC = ask_int_or_default(std::cin, -1);
+		game_options.number_of_characters_per_code = numberC;
+
+		cout << "Updated ! \n";
+
+		return game_options;
+	}
+
+	GameOptions change_Char_Min(GameOptions& game_options)
+	{
+
+		cout << "Which characters do you want to have as min character ? \n";
+		const char char_min = ask_char_or_default(std::cin, -1);
+		game_options.minimum_allowed_character = char_min;
+		cout << "Updated ! \n";
+
+		return game_options;
+	}
+
+	GameOptions change_Char_Max(GameOptions& game_options)
+	{
+
+		cout << "Which characters do you want to have as max character ? \n";
+		const char char_max = ask_char_or_default(std::cin, -1);
+		game_options.maximum_allowed_character = char_max;
+		cout << "Updated ! \n";
+
+		return game_options;
+	}
+
+
+
+	void  game_options_menu(GameOptions& game_options)
+	{
+		bool stay = true;
+		
+		
+		do {
+
+			cout << "\n Which game option do you want to change ? ";
+			cout << "\n 1) Number of characters per code ? ";
+			cout << " \n 2) Character min ? ";
+			cout << "\n 3) Character max ? \n ";
+
+			const auto user_choice = ask_option_menu_choice(std::cin);
+			switch (user_choice)
+			{
+			case OptionMenuChoice::Quit:
+				cout << "Exit \n";
+				stay = false;
+				return;
+			case OptionMenuChoice::NumberOfCharacters:
+				change_Number_Characters(game_options);
+				break;
+			case OptionMenuChoice::CharMin:
+				change_Char_Min(game_options);
+				break;
+			case OptionMenuChoice::CharMax:
+				change_Char_Max(game_options);
+				break;
+			case OptionMenuChoice::Error:
+				std::cout << "\nYou did not enter a valid choice, please try again\n";
+				break;
+			}
+
+
+		} while (stay);
+		
+
+			
+	
+	}
+
+
+
     void configure_game_options(GameOptions& game_options)
     {
-        std::cout << "TODO:\n"
-                     "    DO\n"
-                     "       Display the current game options\n"
-                     "       Display the game options menu\n"
-                     "       Ask the user to type its choice\n"
-                     "       Treat the user's choice\n"
-                     "    UNTIL user's choice is to go back to main menu\n";
+		bool stay = true;
+		do {
+
+			std::cout << "\n TODO:\n"
+				"    DO\n"
+				"       Display the current game options\n"
+				"       Display the game options menu\n"
+				"       Ask the user to type its choice\n"
+				"       Treat the user's choice\n"
+				"    UNTIL user's choice is to go back to main menu\n";
+			
+
+			const auto user_choice = ask_configuration_menu_choice(std::cin);
+			switch (user_choice)
+			{
+			case ConfigureOptions::Quit:
+				cout << "Exit \n";
+				stay = false;
+				return;
+			case ConfigureOptions::CurrentGameOptions:
+				display_current_game_options(game_options);
+				break;
+			case ConfigureOptions::GameOptionMenu:
+				game_options_menu(game_options);
+				break;
+
+			case  ConfigureOptions::Error:
+				std::cout << "\nYou did not enter a valid choice, please try again\n";
+				break;
+			}
+		} while (stay);
     }
 
     void play_game()
--- src/bulls_and_cows_lib/game.hpp
+++ src/bulls_and_cows_lib/game.hpp
@@ -1,8 +1,60 @@
 
 #pragma once
 
+#include <vector> 
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <thread>
+
 namespace bulls_and_cows {
 
     void play_game();
+	/*struct FinalBoard
+	{
+		string secretCode;
+		unsigned int bulls;
+		unsigned int cows;
+		
+		// constructeur
+		FinalBoard(string _secretCode, unsigned int _bulls, unsigned int _cows)
+		{
+			secretCode = _secretCode;
+			bulls = _bulls;
+			cows = _cows;
+		}
 
-} // namespace bulls_and_cows
+	};*/
+
+	struct FinalBoard
+	{
+		std::string secretCode{};
+		unsigned int bulls{};
+		unsigned int cows{};
+		FinalBoard(std::string _secretCode, unsigned int _bulls, unsigned int _cows)
+			: secretCode(_secretCode), bulls(_bulls), cows(_cows)
+		{}
+	};
+
+	enum class OptionMenuChoice
+	{
+		Error = -1,
+		Quit = 0,
+		NumberOfCharacters = 1,
+		CharMin = 2,
+		CharMax = 3,
+	};
+
+
+	enum class ConfigureOptions
+	{
+		Error = -1,
+		Quit = 0,
+		CurrentGameOptions = 1,
+		GameOptionMenu = 2,
+
+	};
+
+
+
+} // namespace bulls_and_cow
