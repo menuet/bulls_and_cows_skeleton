650b340e5e41d1290d5b60c6f6a014016dcc3d7e
sebastien92160 <62285640+sebastien92160@users.noreply.github.com>
2020-03-26 22:34:07 +0100

    Sebastien Trehet commit game_options.cpp

--- src/bulls_and_cows_lib/game_options.cpp
+++ src/bulls_and_cows_lib/game_options.cpp
@@ -1,8 +1,115 @@
 
 #include "game_options.hpp"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
+#include <sstream>
 
+using namespace std; 
 namespace bulls_and_cows {
 
-    // TODO: define the body of the functions declared in game_options.cpp
+    //fonction qui permet de diviser un string en vecteur de string. on l utilise dans load_game_options
+    vector<string> split(string strToSplit, char delimeter)
+    {
+        stringstream ss(strToSplit);
+        string item;
+        vector<string> splittedStrings;
+        while (getline(ss, item, delimeter))
+        {
+            splittedStrings.push_back(item);
+        }
+        return splittedStrings;
+    }
+
+   bool save_game_options(std::ostream& output_file_stream, const GameOptions& game_options)
+    {
+       string Path;
+       cout << "Choisissez le chemin de sauvegarde du fichier" << endl;
+       cin >> Path;
+
+       string Data = "max_number_of_attempts=" + to_string(game_options.max_number_of_attempts) + "\n";
+       Data += "number_of_characters_per_code=" + to_string(game_options.number_of_characters_per_code) + "\n";
+       Data += "minimum_allowed_character=" + to_string(game_options.minimum_allowed_character) + "\n";
+       Data += "maximum_allowed_character=" + to_string(game_options.maximum_allowed_character) + "\n";
+
+       output_file_stream << Data; // pour valider le test
+       ofstream myfile;
+       myfile.open(Path);
+       myfile << Data;
+       myfile.close();
+
+       return true;
+   }
+
+      bool load_game_options(std::istream& input_file_stream, GameOptions& game_options)
+   {
+          string Path;
+          cout << "Choisissez le chemin du fichier a importer" << endl;
+          cin >> Path;
+
+          ifstream inFile;
+          inFile.open(Path);
+
+          if (!inFile)
+          {
+              cerr << "Impossible d'ouvrir ce fichier";
+              return false;
+          }
+          string str;
+              while (getline(inFile, str))
+          {
+              vector<string> tokens = split(str, '=');
+              if (tokens[0] == "max_number_of_attempts")
+              {
+                  game_options.max_number_of_attempts = stoi(tokens[1]);
+              }
+              else if (tokens[0] == "number_of_characters_per_code")
+              {
+                  game_options.number_of_characters_per_code = stoi(tokens[1]);
+              }
+              else if (tokens[0] == "minimum_allowed_character")
+              {
+                  game_options.minimum_allowed_character = (char)(stoi(tokens[1])); //    stoi("65") -> 65 | (char)65 -> 'A'   
+              }
+              else if (tokens[0] == "maximum_allowed_character")
+              {
+                  game_options.maximum_allowed_character = (char)(stoi(tokens[1]));
+              }
+              
+          }
+
+       return true;
+   }
+
+   
+   GameOptionsMenuChoice ask_game_options_menu_choice(std::istream& input_stream)
+   {
+       int x;
+       input_stream >> x;
+       GameOptionsMenuChoice choice = (GameOptionsMenuChoice)x;
+       return choice;
+   }
+
+      void display_game_options(std::ostream& output_stream, const GameOptions& game_options)
+   {
+          output_stream << "VOICI LES GAME OPTIONS:" << endl;
+       output_stream << " max_number_of_attempts = " << game_options.max_number_of_attempts << endl;
+          output_stream << " number_of_characters_per_code = " << game_options.number_of_characters_per_code << endl;
+       output_stream << " minimum_allowed_character = " << game_options.minimum_allowed_character << endl;
+          output_stream << " maximum_allowed_character = " << game_options.maximum_allowed_character << endl;
+      }
+
+      void display_game_options_menu(std::ostream& output_stream)
+      {
+          output_stream << "que voulez vous changer ? " << endl;
+          output_stream << "0 - retour au jeu " << endl;
+          output_stream << "1 - max_number_of_attempts" << endl;
+          output_stream << "2 - number_of_characters_per_code" << endl;
+          output_stream << "3 - minimum_allowed_character" << endl;
+          output_stream << "4 - maximum_allowed_character" << endl;
+          output_stream << "5 - sauvegarder les options actuelle" << endl;
+          output_stream << "6 - charger des options pres enregistrer" << endl;
+      }
 
 } // namespace bulls_and_cows
