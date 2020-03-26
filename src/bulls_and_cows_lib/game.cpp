8282d62b54c98538bbd933eef81a09541f072c93
sebastien92160 <62285640+sebastien92160@users.noreply.github.com>
2020-03-26 22:31:18 +0100

    Sebastien Trehet commit game.cpp

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -8,22 +8,117 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <thread>
+using namespace std; // lifehack
 
 namespace bulls_and_cows {
 
+    void configure_game_options(GameOptions& game_options)
+    {
+        bool wantChange = true;
+        while (wantChange)
+        {
+            display_game_options(cout, game_options);
+            display_game_options_menu(cout);
+
+            /*int choice;
+            cin >> choice;*/
+            GameOptionsMenuChoice choice = ask_game_options_menu_choice(cin);
+            switch ((int)choice)
+            {
+            case 0:
+                wantChange = false;
+                break;
+            case 1:
+                cout << "valeur?" << endl;
+                cin >> game_options.max_number_of_attempts;
+                break;
+            case 2:
+                cout << "valeur?" << endl;
+                cin >> game_options.number_of_characters_per_code;
+                break;
+            case 3:
+                cout << "valeur?" << endl;
+                cin >> game_options.minimum_allowed_character;
+                break;
+            case 4:
+                cout << "valeur?" << endl;
+                cin >> game_options.maximum_allowed_character;
+                break;
+            case 5:
+                save_game_options(cout, game_options);
+                break;
+            case 6:
+                load_game_options(cin, game_options);
+                break;
+            default:
+                break;
+            }
+        }
+
+        std::cout << "TODO:\n"
+                     "    DO\n"
+                     "       Display the current game options\n"
+                     "       Display the game options menu\n"
+                     "       Ask the user to type its choice\n"
+                     "       Treat the user's choice\n"
+                     "    UNTIL user's choice is to go back to main menu\n";
+    }
+
     void user_plays_against_computer(const GameOptions& game_options)
     {
-        std::cout << "TODO:\n"
-                     "    Create a board with a randomly generated secret code\n"
-                     "    DO\n"
-                     "       Display the board and the list of attempts so far\n"
-                     "       Ask the user to make another attempt\n"
-                     "       Compare the user's attempt with the secret code and deduce the number of bulls and cows\n"
-                     "       Add the user's attempt to the list of attempts of the board\n"
-                     "    WHILE not end of game\n"
-                     "    Display the board and the list of attempts so far\n"
-                     "    Display a message telling if the user won or lost\n";
+        /*
+            Pseudo Code:
+
+
+            Creer le board et les games_options
+            Tant que le jeu n'est pas fini
+                afficher l'état actuelle du BAILLE
+                demander à l'U un attempt
+                process l'attempt (en faire un feedback, l'ajouter au board etc...)
+            Fin Tant que
+            afficher l'état final (réussi ou perdu)
+            ptit message sympa
+        */
+
+       // GameOptions game_option{};
+       /* cout << "voulez vous changez les options?" << endl;
+        cout << "1 - oui" << endl;
+        cout << "2 - non" << endl;
+        int reqponse;
+        cin >> reqponse;
+        if (reqponse == 1)
+        {
+            configure_game_options(game_options);
+        }*/
+        Board board = create_board(game_options);
+        // std::cout << " le mot de passe est : " << board.secret_code.value << std::endl;
+        while (!is_end_of_game(game_options, board))
+        {
+            display_board(std::cout, game_options, board); // bug
+            Code codex = ask_attempt(std::cout, std::cin, game_options, board);
+            Feedback feedback = compare_attempt_with_secret_code(codex, board.secret_code);
+            AttemptAndFeedback aaf;
+            aaf.attempt = codex;
+            aaf.feedback = feedback;
+            board.attempts_and_feedbacks.push_back(aaf);
+        }
+        display_board(std::cout, game_options, board);
+        std::cout << "FINI" << std::endl;
+        if (is_win(game_options, board))
+        {
+            std::cout << " bien joue, tu as gagne!" << std::endl;
+        }
+        else
+        {
+            std::cout << " dommage, tu as perdu!" << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
     }
 
     void computer_plays_against_computer(const GameOptions& game_options)
@@ -44,17 +139,6 @@ namespace bulls_and_cows {
                "    WHILE not end of game\n"
                "    Display the board and the list of attempts so far\n"
                "    Display a message telling if the computer won or lost\n";
-    }
-
-    void configure_game_options(GameOptions& game_options)
-    {
-        std::cout << "TODO:\n"
-                     "    DO\n"
-                     "       Display the current game options\n"
-                     "       Display the game options menu\n"
-                     "       Ask the user to type its choice\n"
-                     "       Treat the user's choice\n"
-                     "    UNTIL user's choice is to go back to main menu\n";
     }
 
     void play_game()
