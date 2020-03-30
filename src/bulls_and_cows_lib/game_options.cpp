798d155b679868cacae98995eaed5a236d7c560b
Ahmad <ahmad.jaber@efrei.net>
2020-03-30 20:33:50 +0200

    modification + affichage

--- src/bulls_and_cows_lib/game_options.cpp
+++ src/bulls_and_cows_lib/game_options.cpp
@@ -1,130 +1,130 @@
-
 #include "game_options.hpp"
+#include "input.hpp"
 
 namespace bulls_and_cows {
 
-    // TODO: define the body of the functions declared in game_options.cpp
 
+	// TODO: define the body of the functions declared in game_options.cpp
 	void display_game_options(std::ostream& output_stream, const GameOptions& game_options)
 	{
-
-		output_stream << " unsigned int max_number_of_attempts\n"
-			" number_of_characters_per_code \n"
-			" minimum_allowed_character \n"
-			" maximum_allowed_character";
-		output_stream << "These are the game options\n"
+		output_stream << "\n#################################\n"
+			<< "Here are the current game_options:\n"
 			<< "Maximum number of attempts per game: " << game_options.max_number_of_attempts << "\n"
 			<< "Number of characters in a code: " << game_options.number_of_characters_per_code << "\n"
-			<< "Range of allowed characters: " << game_options.minimum_allowed_character
-			<< "up to " << game_options.maximum_allowed_character << "\n";
-
+			<< "Range of allowed characters: from '" << game_options.minimum_allowed_character << "' to '"
+			<< game_options.maximum_allowed_character << "'\n";
 	}
+
 	void display_game_options_menu(std::ostream& output_stream)
 	{
-		output_stream << "Here are the game options\n"
-			"0 - BackToMain\n"
-			"1 - Modify the maximum number of attempts\n"
-			"2 - Modify the number of characters per code\n"
-			"3 - Modify the minimum allowed of characters\n"
-			"4 - Modify the maximum allowed of characters\n"
-			"5 - Save options\n"
-			"6 - Load options\n"
-			"What is your choice ? ";
-		<< "0 - BackToMain\n"
-			<< "1 - Modify the maximum number of attempts\n"
-			<< "2 - Modify the number of characters per code\n"
-			<< "3 - Modify the minimum allowed of characters\n"
-			<< "4 - Modify the maximum allowed of characters\n"
-			<< "5 - Save options\n"
-			<< "6 - Load options\n"
-			<< "What is your choice ? ";
+		output_stream << "\n#################################\n"
+			<< "0-Back to main menu\n"
+			<< "1-Modify Maximum number of attemps per game\n"
+			<< "2-Modify Number of characters in a code\n"
+			<< "3-Modify Minimum allowed character\n"
+			<< "4-Modify Maximum allowed character\n"
+			<< "5-Save options\n"
+			<< "6-Load options\n";
 	}
+
 	GameOptionsMenuChoice ask_game_options_menu_choice(std::istream& input_stream)
 	{
-		const int user_choice = ask_int_or_default(input_stream, -1);
-		int user_choice{};
-		GameOptionsMenuChoice input{};
-		user_choice = ask_int_or_default(input_stream, -1);
-
-		switch (user_choice)
+		switch (ask_int_or_default(input_stream, -1))
 		{
-		case -1:
-			input = GameOptionsMenuChoice::Error;
-			break;
 		case 0:
 			return GameOptionsMenuChoice::BackToMain;
-			input = GameOptionsMenuChoice::BackToMain;
-			break;
+
+
+
 		case 1:
 			return GameOptionsMenuChoice::ModifyMaximumNumberOfAttempts;
-			input = GameOptionsMenuChoice::ModifyMaximumNumberOfAttempts;
-			break;
+
+
+
 		case 2:
 			return GameOptionsMenuChoice::ModifyNumberOfCharactersPerCode;
-			input = GameOptionsMenuChoice::ModifyNumberOfCharactersPerCode;
-			break;
+
+
+
 		case 3:
 			return GameOptionsMenuChoice::ModifyMinimumAllowedCharacter;
-			input = GameOptionsMenuChoice::ModifyMinimumAllowedCharacter;
-			break;
+
+
+
 		case 4:
 			return GameOptionsMenuChoice::ModifyMaximumAllowedCharacter;
-			input = GameOptionsMenuChoice::ModifyMaximumAllowedCharacter;
-			break;
+
+
+
 		case 5:
 			return GameOptionsMenuChoice::SaveOptions;
-			input = GameOptionsMenuChoice::SaveOptions;
-			break;
+
+
+
 		case 6:
 			return GameOptionsMenuChoice::LoadOptions;
-			input = GameOptionsMenuChoice::LoadOptions;
-			break;
+
+
+
+		default: return GameOptionsMenuChoice::Error;
 		}
-		return GameOptionsMenuChoice::Error;
-		return input;
 	}
+
+
 
 	bool save_game_options(std::ostream& output_file_stream, const GameOptions& game_options)
 	{
-		//const bulls_and_cows::GameOptions game_options{};
-		const auto result = GameOptionsMenuChoice::SaveOptions;
-		result;
-		output_file_stream <<
-			"max_number_of_attempts=12\n"
-			"number_of_characters_per_code=5\n"
-			"minimum_allowed_character=A\n"
-			"maximum_allowed_character=H\n";
-		output_file_stream << "max_number_of_attempts =" << game_options.max_number_of_attempts
-			<< "\n"
-			"number_of_characters_per_code ="
-			<< game_options.number_of_characters_per_code
-			<< "\n"
-			"minimum_allowed_character ="
-			<< game_options.minimum_allowed_character
-			<< "\n"
-			"maximum_allowed_character="
-			<< game_options.maximum_allowed_character << "\n";
+		output_file_stream << "max_number_of_attempts=" << game_options.max_number_of_attempts << "\n"
+			<< "number_of_characters_per_code=" << game_options.number_of_characters_per_code << "\n"
+			<< "minimum_allowed_character=" << game_options.minimum_allowed_character << "\n"
+			<< "maximum_allowed_character=" << game_options.maximum_allowed_character << "\n";
 		return true;
 	}
+
+
+
 	bool load_game_options(std::istream& input_file_stream, GameOptions& game_options)
 	{
-		//bulls_and_cows::GameOptions game_options{};
-		const auto result = GameOptionsMenuChoice::LoadOptions;
-		result;
-		game_options.max_number_of_attempts == 5;
-		game_options.number_of_characters_per_code == 3;
-		game_options.minimum_allowed_character == '1';
-		game_options.maximum_allowed_character == '8';
-		std::string result;
-		std::getline(input_file_stream, result);
+		std::string line;
+		while (std::getline(input_file_stream, line))
+		{
+			std::size_t delimiter = line.find("=");
+			std::string token = line.substr(0, delimiter);
+			std::string numb = line.substr(delimiter + 1);
+			if (numb != "")
+			{
+				if (token == "max_number_of_attempts")
+					game_options.max_number_of_attempts = std::atoi(numb.c_str());
+				else if (token == "number_of_characters_per_code")
+					game_options.number_of_characters_per_code = std::atoi(numb.c_str());
 
-		//game_options.max_number_of_attempts == 5;
-		//game_options.number_of_characters_per_code == 3;
-		//game_options.minimum_allowed_character == '1';
-		//game_options.maximum_allowed_character == '8';
+
+
+				else if (token == "minimum_allowed_character")
+				{
+					game_options.minimum_allowed_character = numb[0];
+				}
+
+
+
+				else if (token == "maximum_allowed_character")
+				{
+					game_options.maximum_allowed_character = numb[0];
+				}
+				else
+				{
+					std::cout << "Error, file corrupted\n";
+					break;
+				}
+			}
+			else
+			{
+				std::cout << "Error, file corrupted\n";
+				break; // nous fait sortir de l'itération du while
+			}
+		}
 		return true;
 	}
+} 
 
-}// namespace bulls_and_cows
-
-} // namespace bulls_and_cows
+// namespace bulls_and_cows
