9c5f8a7526f81680d28058d9f858bdab8b437dbd
AntoineR <62300715+ddo00@users.noreply.github.com>
2020-03-31 02:10:41 +0200

    Antoine R. option 1
    L'option 3 n'est pas finie.
    
    l'avancement de l'option 3 n'est pas présent dans ce commit pour une meilleur clarté du code 
    
    J'ai tenu compte de vos remarques:
    -j'ai éviter autant que possible d'utiliser using namespace, surtout pour le namespace std.
    -j'ai effectivement remarquer un bug pour le compteur des Cows, il est corrigé
    -le test de sortie de la boucle prend en compte le cas où le joueur dépasse le nombre de tentatives autorisées
    -Le code qui calcule les Bulls et les Cows sont maintenant dans une fonction

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -9,63 +9,167 @@
 #include <fstream>
 #include <iostream>
 #include <thread>
-using namespace std;
+#include <string>
+#include <stdlib.h> 
+#include <time.h>
+#include "random.hpp"
+#include <random>
+
+
 namespace bulls_and_cows {
 
+    
+
+
+    std::string create_code(const GameOptions& g1)// fonction qui retourne le code secret en fonction des games options( prend en parametre la reference d'une structure GameOptions g1 )
+    {
+        std::string code_secret;
+        char Charact{};
+        for (int unsigned i = 0; i < g1.number_of_characters_per_code; i++)
+        {
+            Charact = generate_random_character(g1.minimum_allowed_character,
+                                                     g1.maximum_allowed_character);
+           
+            // ajoute le caractere généré de facon random dans le string code (condition si le nombre de caractere atteint celui du game option)
+            code_secret.push_back(Charact);
+        }
+        return code_secret;
+    }
+    
+
+    int compteur_bulls(std::string code_secret, std::string code_test, bool secret_charact_used[],
+                       bool test_charact_used[])
+    {
+        int n = 0; //le compteur
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (code_secret[i] == code_test[i])
+            {
+                n++;
+                // on note les characteres utilisées
+                secret_charact_used[i] = true;
+                test_charact_used[i] = true;
+            }
+        }
+
+        return n;
+    }
+
+    int compteur_cows(std::string code_secret, std::string code_test, bool secret_charact_used[],
+                      bool test_charact_used[])
+    {
+        int n = 0;//le compteur
+
+        
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                // ne pas utiliser un charatere deja utilisé
+                if (secret_charact_used[i] || test_charact_used[j])
+                    continue;
+
+                if (code_secret[i] == code_test[j])
+                {
+                    n++;
+                    // on note les characteres utilisées
+                    secret_charact_used[i] = true;
+                    test_charact_used[j] = true;
+                }
+            }
+        }
+
+        return n;
+    }
+
+    bool secure_saisie(std::string const& code, const GameOptions& g1) //retourne un bouleen en fonction de la validité de la saisie
+    {
+        
+
+        if (code.size() != g1.number_of_characters_per_code)
+        {
+            std::cout << "Le nombre de charactere est INVALIDE, recommencez : \n";
+            
+            return false;// retourne faux si le nombre de caractere est invalide
+        }
+
+        for (const char& i : code)
+        {
+            if (i > g1.maximum_allowed_character || i < g1.minimum_allowed_character)
+            {
+                std::cout << "characteres non defini par les games option  : \n";
+
+                return false;// retourne faux si les  caracteres sont hors range
+            }
+        }
+        return true;
+    }
+
+    std::string input(const GameOptions& g1)
+    {
+        std::string code;
+        do
+        {
+            std::cout << "Quel est votre tentative: ";
+            std::cin >> code;
+           
+        } while (!secure_saisie(code, g1)); // tant que le code n'est pas valide avec une saisie secure
+        return code;
+
+    }
     void user_plays_against_computer(const GameOptions& game_options)
     {
-       /* std::cout << "TODO:\n"
-                     "    Create a board with a randomly generated secret code\n"
-                     "    DO\n"
-                     "       Display the board and the list of attempts so far\n"
-                     "       Ask the user to make another attempt\n"
-                     "       Compare the user's attempt with the secret code and deduce the number of bulls and cows\n"
-                     "       Add the user's attempt to the list of attempts of the board\n"
-                     "    WHILE not end of game\n"
-                     "    Display the board and the list of attempts so far\n"
-                     "    Display a message telling if the user won or lost\n";*/
+        const GameOptions g1 = {
+            3,   //nb de tentative 
+            5,    // charactere max
+            'A', // low range
+            'H' // max range
+        };
+        //generation du code aléatoire
+        std::string code_ordi = create_code(g1);
+        //std::cout << code_ordi << std::endl; 
+      
 
-        string code = "AEFGH"; // TODO generation aleatoire
-        string test;
-        int i_bulls = 0;
-        int i_cows = 0;
+        std::cout << "Touvez le code secret de l'ordi" << std::endl;
+        std::cout << "Jouons...! " << std::endl;
 
-        while (i_bulls != 5)
+        unsigned int essais = 0;
+        while (essais!=g1.max_number_of_attempts)
         {
-            i_bulls = 0;
-            i_cows = 0;
+            // l'utilisateur saisit son essai
+            std::string test = input(g1);
+            essais++;
 
-            cout << "\nType your guess: "; // TODO securiser la saisie (Entre A Et H, 5 charactere obligatoire)
-            cin >> test;
-            cout << "\nYour guess is: " << test;
+            // Pour marquer les caracteres qui ont ete utiliser
+            bool secret_charact_used[5] = {false, false, false, false, false};
+            
+            bool test_charact_used[5] = {false, false, false, false, false};
 
-            // compteur de bulls
-            for (int i = 0; i < 5; i++)
+            
+            int bulls = compteur_bulls(code_ordi, test, secret_charact_used, test_charact_used);
+
+            // condition de victoire
+            if (bulls == 5)
             {
-                if (code[i] == test[i])
-                {
-                    i_bulls++;
-                }
-            }
-            // compteur de cows
-            for (int k = 0; k < 5; k++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    if (code[k] == test[j])
-                    {
-                        i_cows++;
-                    }
-                }
+                std::cout << "Felicitation!!!" << std::endl;
+                std::cout << "Vous avez trouver le code secret en  " << essais << " tentatives" << std::endl;
+                break;
             }
 
-            cout << "\nnombre de bulls  : \n";
-            cout << i_bulls;
-            cout << "\nnombre de cows  : \n";
-            cout << i_cows;
+            
+            int cows = compteur_cows(code_ordi, test, secret_charact_used, test_charact_used);
+
+            // nouvelle tentatve
+            std::cout << "Tentative " << essais << ": Votre code " << test << " possede " << bulls << " bulls et "
+                      << cows << " cows." << std::endl;
         }
-        cout << "\n FELICITATION !!! \n"; 
-         
+
+        // defaite
+        std::cout << "Perdu... " << std::endl;
+        std::cout << "Le code secret est " << code_ordi << std::endl;
+
+        
     }
 
     void computer_plays_against_computer(const GameOptions& game_options)
@@ -98,13 +202,16 @@ namespace bulls_and_cows {
 
     void configure_game_options(GameOptions& game_options)
     {
-        std::cout << "TODO:\n"
+       /* std::cout << "TODO:\n"
                      "    DO\n"
                      "       Display the current game options\n"
                      "       Display the game options menu\n"
                      "       Ask the user to type its choice\n"
                      "       Treat the user's choice\n"
-                     "    UNTIL user's choice is to go back to main menu\n";
+                     "    UNTIL user's choice is to go back to main menu\n";*/
+
+      
+
     }
 
     void play_game()
