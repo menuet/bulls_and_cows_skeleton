074185a3863bc977497fb726c954d8aa51aaae90
Paul-Antoine Inghelbrecht <paulantoine00@orange.fr>
2020-04-12 23:50:29 +0200

    test

--- src/bulls_and_cows_lib/game_solver.hpp
+++ src/bulls_and_cows_lib/game_solver.hpp
@@ -7,16 +7,15 @@
 namespace bulls_and_cows {
 
 	void returnAllCombination(std::vector<std::string>& allPossibilities, std::string sequence,
-                              unsigned int nbrCaractere, unsigned int nbrCaractereCode,
-                              const GameOptions& game_options);
+                              unsigned int nbrCaractereCode, const GameOptions& game_options,
+                              const std::string& allPossibleLetters);
 
     std::vector<std::string> createAllPossibilities(const GameOptions& game_options);
 
     std::string randomInPossibilities(const std::vector<std::string>& allPossibilities);
 
         void deleteIncorrectPoissibilities(std::vector<std::string>& allPossibilities, const std::string& codeComputer,
-                                       const unsigned int bulls, const unsigned int cows,
-                                       const GameOptions& game_options);
+                                       const unsigned int bulls, const unsigned int cows);
 
 
 } // namespace bulls_and_cows
