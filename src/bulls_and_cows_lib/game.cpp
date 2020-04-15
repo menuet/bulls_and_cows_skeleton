3caa234c3c437602537f080ae6897b96667db3d9
AntoineR <62300715+ddo00@users.noreply.github.com>
2020-04-15 22:03:44 +0200

    Add files via upload
    option 2

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -14,6 +14,7 @@
 #include <time.h>
 #include "random.hpp"
 #include <random>
+#include <vector>
 
 
 namespace bulls_and_cows {
@@ -37,7 +38,7 @@ namespace bulls_and_cows {
     }
     
 
-    int compteur_bulls(std::string code_secret, std::string code_test, bool secret_charact_used[],
+   /* int compteur_bulls(std::string code_secret, std::string code_test, bool secret_charact_used[],
                        bool test_charact_used[])
     {
         int n = 0; //le compteur
@@ -54,9 +55,23 @@ namespace bulls_and_cows {
         }
 
         return n;
+    }*/
+
+    
+    int compteur_bullsV2(std::string code_secret, std::string code_test)
+    {
+        int n = 0; // le compteur
+        for (int i = 0; i < code_test.size(); i++)
+        {
+            if (code_secret[i] == code_test[i])
+            {
+                n++;
+            }
+        }
+        return n;
     }
 
-    int compteur_cows(std::string code_secret, std::string code_test, bool secret_charact_used[],
+   /* int compteur_cows(std::string code_secret, std::string code_test, bool secret_charact_used[],
                       bool test_charact_used[])
     {
         int n = 0;//le compteur
@@ -80,6 +95,35 @@ namespace bulls_and_cows {
             }
         }
 
+        return n;
+    }*/
+
+    unsigned int compteur_cowsV2(std::string code_secret, std::string code_test)
+    {
+        std::string code_temp = code_secret;
+        //  ne pas utiliser un charatere deja utilisé (les bulls)
+        for (int i = 0; i < code_test.size(); i++)
+        {
+            if (code_secret[i] == code_test[i])
+            {
+                code_test[i] = '1';
+                code_temp[i] = '0';
+            }
+        }
+        int n = 0; // le compteur
+        for (int i = 0; i < code_test.size(); i++)
+        {
+            for (int j = 0; j < code_temp.size(); j++)
+            {
+                if (code_test[i] == code_temp[j] && i != j)
+                {
+                    n++;
+                    // on note les characteres utilisées
+                    code_test[i] = '1';
+                    code_temp[j] = '0';
+                }
+            }
+        }
         return n;
     }
 
@@ -118,16 +162,12 @@ namespace bulls_and_cows {
         return code;
 
     }
+
     void user_plays_against_computer(const GameOptions& game_options)
     {
-        const GameOptions g1 = {
-            3,   //nb de tentative 
-            5,    // charactere max
-            'A', // low range
-            'H' // max range
-        };
+       
         //generation du code aléatoire
-        std::string code_ordi = create_code(g1);
+        std::string code_ordi = create_code(game_options);
         //std::cout << code_ordi << std::endl; 
       
 
@@ -135,22 +175,22 @@ namespace bulls_and_cows {
         std::cout << "Jouons...! " << std::endl;
 
         unsigned int essais = 0;
-        while (essais!=g1.max_number_of_attempts)
+        while (essais != game_options.max_number_of_attempts)
         {
             // l'utilisateur saisit son essai
-            std::string test = input(g1);
+            std::string test = input(game_options);
             essais++;
-
+            /* V1 des compteurs
             // Pour marquer les caracteres qui ont ete utiliser
-            bool secret_charact_used[5] = {false, false, false, false, false};
+            bool secret_charact_used[5]= {false, false, false, false, false};
             
-            bool test_charact_used[5] = {false, false, false, false, false};
+            bool test_charact_used[5] = {false, false, false, false, false};*/
 
-            
-            int bulls = compteur_bulls(code_ordi, test, secret_charact_used, test_charact_used);
+            //int bulls = compteur_bulls(code_ordi, test, secret_charact_used, test_charact_used);
+            unsigned int bulls = compteur_bullsV2(code_ordi, test);
 
             // condition de victoire
-            if (bulls == 5)
+            if (bulls == game_options.number_of_characters_per_code)
             {
                 std::cout << "Felicitation!!!" << std::endl;
                 std::cout << "Vous avez trouver le code secret en  " << essais << " tentatives" << std::endl;
@@ -158,7 +198,8 @@ namespace bulls_and_cows {
             }
 
             
-            int cows = compteur_cows(code_ordi, test, secret_charact_used, test_charact_used);
+            //int cows = compteur_cows(code_ordi, test, secret_charact_used, test_charact_used);
+            int cows = compteur_cowsV2(code_ordi, test);
 
             // nouvelle tentatve
             std::cout << "Tentative " << essais << ": Votre code " << test << " possede " << bulls << " bulls et "
@@ -171,10 +212,137 @@ namespace bulls_and_cows {
 
         
     }
+   
+    //fonction qui prend en reference un vecteur et ajoute toutes les combinaisons possible de caractere possible ici 8 dans le tableau de char
+    //et de longueur k
+    void algo(char alpha[], std::string code, int n, int l, std::vector<std::string>& storage)
+    {
+
+        // on ajoute 1 par 1 les caracteres et on rappelle grace a la fontion suivante, en decrementant (l est egale à l-1) 
+
+        if (l == 0)
+        {
+           
+
+            storage.push_back(code);
+
+            return;
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            std::string newcode;
+
+            // nouveau caractere ajouté
+            newcode = code + alpha[i];
+
+
+            algo(alpha, newcode, n, l - 1, storage);
+        }
+    }
+
+    void generate_all_possibles_codes(char alpha[], int k, int n, std::vector<std::string>& myvecto)// sert à rappeler de maniere recursif algo
+    {
+        algo(alpha, "", n, k, myvecto);
+    }
+
+     void delele_0bulls_0cows(std::vector<std::string>& allcodes, unsigned int bulls, unsigned int cows, std::string test)// fonction qui permet lorque 
+       // q'une tentative a 0bulls et 0cows d'enlever touts les codes avec les meme character que la tentative
+        
+    {
+        size_t data = allcodes.size();
+        int convertdata = static_cast<int>(data);
+
+        int x = convertdata-1;
+        if (bulls == 0 && cows == 0)
+        { 
+            std::string code_temp;
+
+            for (int w = 0; w < x; w++)
+            {
+                data = allcodes.size();
+                convertdata = static_cast<int>(data);
+                x = convertdata - 1;
+                code_temp = allcodes[w];
+                std::cout << "reflexion en cours..." << std::endl;
+
+                for (int i = 0; i < test.size(); i++)
+                {
+                    for (int j = 0; j < code_temp.size(); j++)
+                    {
+                        if (test[i] == code_temp[j])
+                        {
+                            allcodes.erase(allcodes.begin() + w);
+                            
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+        else
+        {
+            
+        }
+    }
+    
 
     void computer_plays_against_computer(const GameOptions& game_options)
     {
-        std::cout
+
+        std::cout << "First Version CPU VS CPU" << std::endl;
+        std::vector<std::string> allcodes;
+        char set1[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}; //coder en dur pour l'instant
+        generate_all_possibles_codes(set1, game_options.number_of_characters_per_code, 8, allcodes);
+        std::string code_ordi = create_code(game_options); // Le code SECRET
+        size_t data = allcodes.size();
+        int convertdata = static_cast<int>(data);
+
+        int size = convertdata; // nombre de codes possibles
+        //int x = (pow(8,5)-1) coder en dur car pb de conversion double en int par la suite
+        unsigned int essais = 0;
+       
+        while (essais != -1)
+        {
+            data = allcodes.size();
+            convertdata = static_cast<int>(data);
+
+            size = convertdata-1; // nombre de codes possibles
+            essais++;
+            int o = generate_random_integer(0, size); // on prend un code possible aleatoirement 
+            std::cout << "Le vrai code est " << code_ordi << std::endl;
+            std::string test = allcodes[o];
+            std::cout << "Le code test est " << test << std::endl;
+
+            unsigned int bulls = compteur_bullsV2(code_ordi, test);
+
+            // condition de victoire
+            if (bulls == game_options.number_of_characters_per_code)
+            {
+                std::cout << "Felicitation!!!" << std::endl;
+                std::cout << "taille du vecteur allcodes: " << allcodes.size() << '\n';
+                break;
+            }
+
+            allcodes.erase(allcodes.begin() + o);// le code testé est faux on le supprime du vecteur
+            
+            
+            unsigned int cows = compteur_cowsV2(code_ordi, test);
+
+
+            delele_0bulls_0cows(allcodes, bulls, cows, test);// fonction treees longue avec qql crash
+            
+
+            std::cout << "Tentative " << essais << ": Votre code " << test << " possede " << bulls << " bulls et "
+                      << cows << " cows." << std::endl;
+        }
+        
+        }
+
+        
+       /* std::cout
             << "TODO:\n"
                "    Create a board with a randomly generated secret code\n"
                "    Generate the list of all the possible codes\n"
@@ -189,7 +357,7 @@ namespace bulls_and_cows {
                "possible codes\n"
                "    WHILE not end of game\n"
                "    Display the board and the list of attempts so far\n"
-               "    Display a message telling if the computer won or lost\n";
+               "    Display a message telling if the computer won or lost\n";*/
 
 
 
@@ -198,7 +366,7 @@ namespace bulls_and_cows {
 
 
 
-    }
+    
 
     void configure_game_options(GameOptions& game_options)
     {
