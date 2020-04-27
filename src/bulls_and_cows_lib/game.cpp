62ffd9fa20f5120dbfd6e5224d026f341e5e895b
Cherif Sidi Mahmoud <chrifmoulaydriss99@gmail.com>
2020-04-27 02:48:17 +0200

    Configuration du jeu

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -48,23 +48,91 @@ namespace bulls_and_cows {
 
     void configure_game_options(GameOptions& game_options)
     {
-        std::cout << "TODO:\n"
+        /*std::cout << "TODO:\n"
                      "    DO\n"
                      "       Display the current game options\n"
                      "       Display the game options menu\n"
                      "       Ask the user to type its choice\n"
                      "       Treat the user's choice\n"
-                     "    UNTIL user's choice is to go back to main menu\n";
+                     "    UNTIL user's choice is to go back to main menu\n";*/
+
+        std::cout << "\n#################################\n";
+
+        display_game_options(std::cout, game_options);
+        display_game_options_menu(std::cout);
+        const auto user_choice = ask_game_options_menu_choice(std::cin);
+        switch (user_choice)
+        {
+        case GameOptionsMenuChoice::BackToMain:
+            play_game();
+        case GameOptionsMenuChoice::ModifyMaximumNumberOfAttempts: {
+            std::cout << "Enter the maximum number of attempts : ";
+            int newVal = ask_int_or_default(std::cin, -1);
+            if (newVal != -1)
+            {
+                game_options.max_number_of_attempts = newVal;
+                configure_game_options(game_options);
+            }
+            break;
+        }
+        case GameOptionsMenuChoice::ModifyNumberOfCharactersPerCode: {
+            std::cout << "Enter the number of caracter per code : ";
+            int newVal1 = ask_int_or_default(std::cin, -1);
+            if (newVal1 != -1)
+            {
+                game_options.number_of_characters_per_code = newVal1;
+                configure_game_options(game_options);
+            }
+            break;
+        }
+        case GameOptionsMenuChoice::ModifyMinimumAllowedCharacter: {
+            std::cout << "Enter the minimum allowed caracter : ";
+            char newVal2 = ask_char_or_default(std::cin, 'x');
+            if (!isdigit(newVal2) && newVal2 != 'x')
+            {
+                game_options.minimum_allowed_character = newVal2;
+                configure_game_options(game_options);
+            }
+            else
+            {
+                configure_game_options(game_options);
+            }
+            break;
+        }
+        case GameOptionsMenuChoice::ModifyMaximumAllowedCharacter: {
+            std::cout << "Enter the maximum allowed caracter : ";
+            char newVal3 = ask_char_or_default(std::cin, 'x');
+            if (!isdigit(newVal3) && newVal3 != 'x')
+            {
+                game_options.maximum_allowed_character = newVal3;
+                configure_game_options(game_options);
+            }
+            else
+            {
+                configure_game_options(game_options);
+            }
+            break;
+        }
+        case GameOptionsMenuChoice::SaveOptions:
+            configure_game_options(game_options);
+            break;
+        case GameOptionsMenuChoice::LoadOptions:
+            configure_game_options(game_options);
+            break;
+        case GameOptionsMenuChoice::Error:
+            std::cout << "\nYou did not enter a valid choice, please try again\n";
+            break;
+        }
     }
 
     void play_game()
     {
         GameOptions game_options{};
+        std::cout << "\n#################################\n";
+        display_main_menu(std::cout);
 
         while (true)
         {
-            std::cout << "\n#################################\n";
-            display_main_menu(std::cout);
 
             const auto user_choice = ask_main_menu_choice(std::cin);
             switch (user_choice)
